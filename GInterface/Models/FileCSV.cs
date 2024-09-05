73f9bf6bfe81f356a4606d12c7d4187d3c0dcaf0
Brayanjax <120285750+Brayanjax@users.noreply.github.com>
2024-09-04 22:30:27 -0600

    Intento de estandarizar cvs
    to do

---
+++ GInterface/Models/FileCSV.cs
@@ -0,0 +1,14 @@
+﻿namespace GInterface.Models
+{
+    public class FileCSV
+    {
+
+        public int ID { get; set; }
+        public string FileNames { get; set; }
+        public DateTime FileDate { get; set; }
+        public int FileStatus { get; set; }
+        public int FileFields { get; set; }
+        public string FileJsonObj { get; set; }
+
+    }
+}
---
+++ GInterface/Models/TempCSVGlobal.cs
@@ -0,0 +1,17 @@
+﻿namespace GInterface.Models
+{
+    public class TempCSVGlobal
+    {
+        public int ID { get; set; }
+        public long Campo1 { get; set; }
+        public long Campo2 { get; set; }
+        public long Campo3 { get; set; }
+        public long Campo4 { get; set; }
+        public long Campo5 { get; set; }
+        public string Campo6 { get; set; }
+        public string Campo7 { get; set; }
+        public string Campo8 { get; set; }
+        public string Campo9 { get; set; }
+        public string Campo10 { get; set; }
+    }
+}

d89880e0ee6e98d30d1254ef34b738e79118551b
Jonatan Segura <jsegura17@hotmail.com>
2024-10-14 11:54:00 -0600

    Se da funcionalidad a boton de logout

--- GInterfaceCore/GInterfaceCore/Interfaces/IAuthenticationService.cs
+++ GInterfaceCore/GInterfaceCore/Interfaces/IAuthenticationService.cs
@@ -3,7 +3,5 @@
     public interface IAuthenticationService
     {
         Task<bool> LoginAsync(string username, string password);
-        Task LogoutAsync();
-        // Otros métodos relacionados con la autenticación
     }
 }

d19d80730345d54b730da448e844c4abd0980b34
Brayanjax <brajo2604@gmail.com>
2024-10-14 12:30:03 -0600

    Cambio de diseño lista del Workspace.razor

---
+++ GInterfaceCore/GInterfaceCore/Hubing/Servicehubs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace GInterfaceCore.Hubing
+{
+    public class Servicehubs
+    {
+        private readonly IHubContext<HubSign> _hubContext;
+
+        public Servicehubs(IHubContext<HubSign> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyClients(string user, string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
